7579d1af18ac0c90e7b39f1bc605a8f6cbd8d416
Kate F <follett.katie@gmail.com>
2019-03-04 16:18:14 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require 'pry'
+
+katz_line = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(array)
+  counter = 1
+  counter +=1
+  if array.length == 0
+    puts "The line is currently empty."
+    elsif array.length > 0
+    puts "The line is currently:" + "#{counter}" + "#{other_deli}" 
+  end
+ 
+end
