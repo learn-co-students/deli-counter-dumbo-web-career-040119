f3b55f3b3a03ccdb35b98784e627b51cf63f20f0
ftse105 <ftse105@gmail.com>
2019-03-17 00:33:07 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+    return
+  end
+  new_array = []
+
+  i = 0
+  while i < array.length
+    char = array[i]
+    position = (i + 1).to_s + ". " + char
+
+    new_array << position
+    i += 1
+  end
+
+  puts "The line is currently: " + new_array.join(" ")
+end
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, " + name + ". You are number " + array.length.to_s + " in line."
+  
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
